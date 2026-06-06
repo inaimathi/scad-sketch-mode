0b216b59d275ff3a31c1b505ca7a023e2bca499d
inaimathi <leo.zovic@gmail.com>
2026-06-06 16:20:48 -0400

    Diagnosing unittest failures

--- unittest.sh
+++ unittest.sh
@@ -1,5 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 emacs --batch -Q \
-  --eval "(progn
-            (dolist (file (directory-files \"tests\" t \"-test\\\\.el\\\\'\"))
+  --eval '(progn
+            (message "Emacs: %s" emacs-version)
+            (message "Load path root: %s" default-directory)
+            (dolist (file (directory-files "tests" t "-test\\.el\\'"))
+              (message "Loading test file: %s" file)
               (load-file file))
-            (ert-run-tests-batch-and-exit))"
+            (ert-run-tests-batch-and-exit))'
