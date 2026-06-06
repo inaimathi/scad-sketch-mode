2005eca895e9b0203e8201c6f6cce41c71673179
inaimathi <leo.zovic@gmail.com>
2026-06-06 16:27:40 -0400

    Fix test runner

--- unittest.sh
+++ unittest.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 emacs --batch -Q \
-  --eval '(progn
-            (message "Emacs: %s" emacs-version)
-            (message "Load path root: %s" default-directory)
-            (dolist (file (directory-files "tests" t "-test\\.el\\'"))
-              (message "Loading test file: %s" file)
+  --eval "(progn
+            (message \"Emacs: %s\" emacs-version)
+            (message \"Default directory: %s\" default-directory)
+            (dolist (file (directory-files \"tests\" t \"-test\\\\.el$\"))
+              (message \"Loading test file: %s\" file)
               (load-file file))
-            (ert-run-tests-batch-and-exit))'
+            (ert-run-tests-batch-and-exit))"
